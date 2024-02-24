a4811e4032bd14c6fb49d89c19726faa2abc33bb
Aaryan Giradkar <aaryang0605@gmail.com>
2024-02-24 11:15:04 +0530

    Password encryption

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,30 +1,40 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
 const User = require("../models/userModel");
-const { get } = require("mongoose");
 
 const app = express.Router();
 
 app.post("/login", async (req, res) => {
-  //Creating reference of user model
   try {
-    const result = await User.findOne({
-      username: req.body.username,
-      password: req.body.password,
-    });
-    if (result) {
-      res.send(result);
-    } else {
-      res.status(400).json("Login failed");
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res.status(400).json("Invalid username or password");
     }
+
+    const validPassword = await bcrypt.compare(password, user.password);
+
+    if (!validPassword) {
+      return res.status(400).json("Invalid username or password");
+    }
+
+    res.send(user);
   } catch (error) {
     res.status(400).json(error);
   }
 });
 
 app.post("/register", async (req, res) => {
-  //Creating reference of user model
   try {
-    const newUser = new User(req.body);
+    const { username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10); // Hash password with 10 salt rounds
+
+    const newUser = new User({
+      username,
+      password: hashedPassword,
+    });
+
     await newUser.save();
     res.send("Registration successful");
   } catch (error) {
@@ -33,7 +43,6 @@ app.post("/register", async (req, res) => {
 });
 
 app.post("/update", async (req, res) => {
-  //Creating reference of user model
   try {
     await User.findOneAndUpdate({ _id: req.body._id }, req.body);
     const user = await User.findOne({ _id: req.body._id });
