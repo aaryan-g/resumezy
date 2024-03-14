47b022a75014fca0e4595c81a572ed42ba15068c
Aaryan Giradkar <aaryang0605@gmail.com>
2024-03-14 15:55:31 +0530

    Production-related Changes

--- server.js
+++ server.js
@@ -2,8 +2,18 @@ const express = require("express");
 const app = express();
 const dbConnect = require("./dbConnect");
 app.use(express.json()); //So that express can destructure JSON
-const port = 5000;
+const port = process.env.PORT || 5000;
 const userRoute = require("./routes/userRoute"); //Creating entry point for userRoute
+const path = require("path");
+
+//For production
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client/build/index.html"));
+  });
+}
 
 //Navigating API to userRoute
 app.use("/api/user/", userRoute);
